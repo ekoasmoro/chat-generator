bc9437d505a13c932e2442ae87294f7c8ad1cec9
Eko Asmoro <ekoasmoro052@gmail.com>
2025-07-30 22:06:20 +0700

    dev awal fitur report

---
+++ assets/js/report.js
@@ -0,0 +1,260 @@
+import {
+  TemplateBugs,
+  TemplateEkspedisi,
+  TemplateCso,
+} from "./templateReport.js";
+
+const selectOperator = document.getElementById("operator");
+const selectShift = document.getElementById("shift");
+const selectTemplate = document.getElementById("pilihTemplate");
+const subTemplateEkspedisi = document.getElementById("subtemplate-ekspedisi");
+const subTemplateBugs = document.getElementById("subtemplate-bugs");
+const subTemplateCso = document.getElementById("subtemplate-cso");
+const getIdReport = document.getElementById("idReport");
+const getIdResi = document.getElementById("idresi");
+const getEmail = document.getElementById("emailAkun");
+const getDeskripsi = document.getElementById("deskripsi");
+const hasiltemplate = document.getElementById("result-box");
+const btnPickup = document.getElementById("copyResult");
+const cpyPop = document.querySelector(".cpy-container");
+const openSubEkspedisi = document.querySelector(".ekspedisi-container");
+const openSubBugs = document.querySelector(".bugs-container");
+const openSubCso = document.querySelector(".cso-container");
+const btnGenerateEkpedisi = document.getElementById("generate-ekspedisi");
+const btnGenerateBugs = document.getElementById("generate-bugs");
+const btnGenerateCso = document.getElementById("generate-cso");
+const btnResetEkpedisi = document.getElementById("reset-ekspedisi");
+const btnResetBugs = document.getElementById("reset-bugs");
+const btnResetCso = document.getElementById("reset-cso");
+const sftSpan = document.getElementById("sft");
+const optSpan = document.getElementById("opt");
+
+
+const shiftGreetingMap = {
+  pagi: "Selamat Pagi",
+  siang: "Selamat Siang",
+  sore: "Selamat Sore",
+  malam: "Selamat Malam",
+};
+
+function updateGreeting() {
+  const shift = selectShift.value;
+  const operator = selectOperator.value;
+
+  sftSpan.textContent = shiftGreetingMap[shift] || "Selamat Datang";
+  optSpan.textContent = operator || "";
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedOperator = localStorage.getItem("selectOperator");
+  const savedShift = localStorage.getItem("selectShift");
+
+  if (savedOperator) selectOperator.value = savedOperator;
+  if (savedShift) selectShift.value = savedShift;
+  updateGreeting()
+});
+
+selectOperator.addEventListener("change", () => {
+  localStorage.setItem("selectOperator", selectOperator.value);
+  updateGreeting()
+});
+
+selectShift.addEventListener("change", () => {
+  localStorage.setItem("selectShift", selectShift.value);
+  updateGreeting()
+});
+
+window.addEventListener("storage", (event) => {
+  if (event.key === "selectOperator") {
+    selectOperator.value = event.newValue || "";
+  }
+
+  if (event.key === "selectShift") {
+    selectShift.value = event.newValue || "";
+  }
+  updateGreeting()
+});
+
+
+const subTemplate = {
+  ekspedisi: [
+    "Push Pengiriman",
+    "Push POD",
+    "Push Return",
+    "Pengiriman Ulang",
+    "Push Resi Masal",
+    "Push Pick Up",
+    "Dokumentasi POD",
+    "Dokumentasi POD Return",
+    "Jemput Paket",
+  ],
+  bugs: [
+    "General Report",
+    "Top Up Kompay Tidak Masuk",
+    "Top Up Kartu Tidak Masuk",
+    "Balance Saldo Tidak Sinkron",
+    "Refund Saldo Belum Masuk",
+    "Gagal Payment",
+    "Gagal Menautkan",
+  ],
+  cso: [
+    "Klaim Tanpa Tiket",
+    "Klaim Hilang",
+    "Push Penarikan",
+    "Penyelesaian Resi",
+  ],
+};
+
+const methodMaps = {
+  ekspedisi: {
+    "Push Pengiriman": "pushDelivery",
+    "Push POD": "pushPod",
+    "Push Return": "pushReturn",
+    "Pengiriman Ulang": "pengirimanUlang",
+    "Push Resi Masal": "pushMasal",
+    "Push Pick Up": "pushPickup",
+    "Dokumentasi POD": "dokumentasiPod",
+    "Dokumentasi POD Return": "podReturn",
+    "Jemput Paket": "jemputPaket",
+  },
+  bugs: {
+    "General Report": "generalReport",
+    "Top Up Kompay Tidak Masuk": "kompayTidakMasuk",
+    "Top Up Kartu Tidak Masuk": "komcardTidakMasuk",
+    "Balance Saldo Tidak Sinkron": "balanceTidakSinkron",
+    "Refund Saldo Belum Masuk": "refund",
+    "Gagal Payment": "gagalPayment",
+    "Gagal Menautkan": "gagalMenautkan",
+  },
+  cso: {
+    "Klaim Tanpa Tiket": "tanpaTiket",
+    "Klaim Hilang": "klaimHilang",
+    "Push Penarikan": "pushPenarikan",
+    "Penyelesaian Resi": "penyelesaianResi",
+  },
+};
+
+const subSelectMap = {
+  ekspedisi: subTemplateEkspedisi,
+  bugs: subTemplateBugs,
+  cso: subTemplateCso,
+};
+
+selectTemplate.addEventListener("change", () => {
+  const kategori = selectTemplate.value;
+
+  openSubEkspedisi.style.display = "none";
+  openSubBugs.style.display = "none";
+  openSubCso.style.display = "none";
+
+  if (kategori === "ekspedisi") openSubEkspedisi.style.display = "block";
+  else if (kategori === "bugs") openSubBugs.style.display = "block";
+  else if (kategori === "cso") openSubCso.style.display = "block";
+
+  const currentSelect = subSelectMap[kategori];
+  if (currentSelect) {
+    currentSelect.innerHTML = `<option value="">-- Pilih Template --</option>`;
+    subTemplate[kategori].forEach((item) => {
+      const option = document.createElement("option");
+      option.value = item;
+      option.textContent = item;
+      currentSelect.appendChild(option);
+    });
+  }
+});
+
+btnGenerateEkpedisi.addEventListener("click", () => {
+  const sub = subTemplateEkspedisi.value;
+  const methodName = methodMaps["ekspedisi"]?.[sub];
+
+  const idReport = getIdReport.value;
+  const idResi = getIdResi.value;
+
+  if (!methodName) return (hasiltemplate.value = "Subtemplate belum dipilih!");
+
+  const instance = new TemplateEkspedisi(idReport, idResi);
+  if (typeof instance[methodName] === "function") {
+    const template = instance[methodName]();
+    hasiltemplate.value = template;
+  } else {
+    hasiltemplate.value = "Metode tidak ditemukan!";
+  }
+});
+
+btnGenerateBugs.addEventListener("click", () => {
+  const sub = subTemplateBugs.value;
+  const methodName = methodMaps["bugs"]?.[sub];
+
+  const idReport = getIdReport.value;
+  const email = getEmail.value;
+  const deskripsi = getDeskripsi.value;
+
+  if (!methodName) return (hasiltemplate.value = "Subtemplate belum dipilih!");
+
+  const instance = new TemplateBugs(idReport, email, deskripsi);
+  if (typeof instance[methodName] === "function") {
+    const template = instance[methodName]();
+    hasiltemplate.value = template;
+  } else {
+    hasiltemplate.value = "Metode tidak ditemukan!";
+  }
+});
+
+btnGenerateCso.addEventListener("click", () => {
+  const sub = subTemplateCso.value;
+  const methodName = methodMaps["cso"]?.[sub];
+
+  const idResi = getIdResi.value;
+
+  if (!methodName) return (hasiltemplate.value = "Subtemplate belum dipilih!");
+
+  const instance = new TemplateCso(idResi);
+  if (typeof instance[methodName] === "function") {
+    const template = instance[methodName]();
+    hasiltemplate.value = template;
+  } else {
+    hasiltemplate.value = "Metode tidak ditemukan!";
+  }
+});
+
+btnResetEkpedisi.addEventListener("click", () => {
+  selectTemplate.value = "";
+  subTemplateEkspedisi.innerHTML = `<option value="">-- Pilih Template --</option>`;
+  getIdReport.value = "";
+  getIdResi.value = "";
+  hasiltemplate.value = "";
+  openSubEkspedisi.style.display = "none";
+});
+
+btnResetBugs.addEventListener("click", () => {
+  selectTemplate.value = "";
+  subTemplateBugs.innerHTML = `<option value="">-- Pilih Template --</option>`;
+  getIdReport.value = "";
+  getEmail.value = "";
+  getDeskripsi.value = "";
+  hasiltemplate.value = "";
+  openSubBugs.style.display = "none";
+});
+
+btnResetCso.addEventListener("click", () => {
+  selectTemplate.value = "";
+  subTemplateCso.innerHTML = `<option value="">-- Pilih Template --</option>`;
+  getIdResi.value = "";
+  hasiltemplate.value = "";
+  openSubCso.style.display = "none";
+});
+
+
+btnPickup.addEventListener("click", () => {
+  hasiltemplate.select();
+  document.execCommand("copy");
+  cpyPop.style.display = "block";
+
+  setTimeout(() => {
+    cpyPop.style.display = "none";
+  }, 2000);
+});
+
+
+
---
+++ assets/js/templateReport.js
@@ -0,0 +1,202 @@
+class TemplateEkspedisi {
+  constructor(idReport, idResi) {
+    this.idReport = idReport;
+    this.idResi = idResi;
+  }
+
+  pushDelivery() {
+    return `Permisi Kak, mohon dibantu untuk push dan maksimalkan proses pengiriman resi berikut karena terindikasi stuck atau tidak update
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+
+Terima kasih`;
+  }
+
+  pushPod() {
+    return `Permisi kak, mohon dibantu cek dan update resi berikut menjadi diterima
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+Deskripsi: Konfirmasi dari penerima paket sudah menerima paketnya
+
+Terima kasih`;
+  }
+
+  pushReturn() {
+    return `Permisi Kak, mohon dibantu untuk push dan maksimalkan proses pengiriman return resi berikut karena terindikasi stuck atau tidak update
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+
+Terima kasih`;
+  }
+
+  pengirimanUlang() {
+    return `Permisi Kak, mohon dibantu untuk proses pengantaran kembali resi berikut
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+Deskripsi: Konfirmasi dari penerima paket, tidak menolak paket dan masih menunggu paketnya untuk diantar
+
+Terima kasih`;
+  }
+
+  pushMasal() {
+    return `Permisi Kak, mohon dibantu untuk push dan maksimalkan proses pengiriman pada daftar resi berikut karena terindikasi stuck atau tidak update
+
+ID Report: ${this.idReport}
+ID Resi: 
+${this.idResi}
+
+Terima kasih`;
+  }
+
+  pushPickup() {
+    return `Permisi Kak, mohon dibantu untuk segera proses pick up, konfirmasi dari pengirim sudah melewati jadwal pick up
+
+Terima kasih`;
+  }
+
+  dokumentasiPod() {
+    return `Permisi Kak, mohon dibantu untuk infokan dokumentasi POD resi berikut, konfirmasi penerima belum menerima paketnya namun statusnya sudah diterima
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+
+Terima kasih`;
+  }
+
+  podReturn() {
+    return `Permisi Kak, mohon dibantu untuk infokan dokumentasi POD return resi berikut, konfirmasi pengirim/seller belum menerima paketnya namun statusnya sudah diterima
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+
+Terima kasih`;
+  }
+
+  jemputPaket() {
+    return `Permisi Kak, mohon dibantu infokan alamat gudang/gerai/agen resi berikut
+
+ID Report: ${this.idReport}
+ID Resi: ${this.idResi}
+Deskripsi: Konfirmasi penerima paket akan melakukan penjemputan paketnya di gudang atau gerai
+
+Terima kasih`;
+  }
+}
+
+class TemplateBugs {
+  constructor(idReport, email, deskripsi) {
+    this.idReport = idReport;
+    this.email = email;
+    this.deskripsi = deskripsi;
+  }
+
+  generalReport() {
+    return `Permisi Tim, mohon dibantu cek kendala berikut
+ID Report: ${this.idReport}
+Email: ${this.email}
+Penjelasan Kendala: ${this.deskripsi}
+
+Terima kasih`;
+  }
+
+  kompayTidakMasuk() {
+    return `Permisi Tim, mohon dibantu cek kendala berikut
+ID Report: ${this.idReport}
+Email: ${this.email}
+Penjelasan Kendala: User mencoba top up ke saldo Kompay namun tidak masuk
+Nominal Top Up: Rp.
+
+Terima kasih`;
+  }
+
+  komcardTidakMasuk() {
+    return `Permisi Tim, mohon dibantu cek kendala berikut
+ID Report: ${this.idReport}
+Email: ${this.email}
+Penjelasan Kendala: User mencoba top up ke kartu Komcard namun tidak masuk atau gagal
+
+Terima kasih`;
+  }
+
+  balanceTidakSinkron() {
+    return `Permisi Tim, mohon dibantu cek kartu berikut karena ada indikasi balance saldo tidak sinkron 
+Email: ${this.email}
+Detail Kartu:
+${this.deskripsi}
+
+Terima kasih`;
+  }
+
+  refund() {
+    return `Permisi Tim, mohon dibantu cek kartu berikut karena karena konfirmasi dari merchant transaksi sebelumnya sudah dilakukan refund
+Email: ${this.email}
+Merchant: 
+Nominal Refund: Rp.
+Detail Kartu:
+${this.deskripsi}
+
+Terima kasih`;
+  }
+
+  gagalPayment() {
+    return `Permisi Tim, mohon dibantu cek kartu berikut karena karena konfirmasi dari user/pengguna mengalami gagal bayar atau transaksinya terdeclined
+Email: ${this.email}
+Merchant: 
+Nominal Tagihan: Rp.
+Detail Kartu:
+${this.deskripsi}
+
+Terima kasih`;
+  }
+
+  gagalMenautkan() {
+    return `Permisi Tim, mohon dibantu cek kartu berikut karena karena konfirmasi dari user/pengguna mengalami gagal menautkan kartunya ke merchant
+Email: ${this.email}
+Merchant: 
+Detail Kartu:
+${this.deskripsi}
+
+Terima kasih`;
+  }
+}
+
+class TemplateCso {
+  constructor(idResi) {
+    this.idResi = idResi;
+  }
+
+  tanpaTiket() {
+    return `Permisi Kak @~Dayu - CSO komhsip , mohon dibantu cek resi berikut dan bantu klaim tanpa tiket 
+ID Resi: ${this.idResi}
+
+Terima kasih`;
+  }
+
+  klaimHilang() {
+    return `Permisi Kak @~Dayu - CSO komhsip , mohon dibantu cek resi berikut karena update dari ekspedisi terindikasi hilang
+ID Resi: ${this.idResi}
+
+Terima kasih`;
+  }
+
+  pushPenarikan() {
+    return `Permisi Kak @~Dayu - CSO komhsip  , mohon dibantu proses penarikan email berikut 
+
+Terima kasih`;
+  }
+
+  penyelesaianResi() {
+    return `Permisi Kak @~Dayu - CSO komhsip , mohon dibantu cek proses penyelesaian kendala resi berikut 
+Email Akun: 
+ID Resi: ${this.idResi}
+Deskripsi: Konfirmasi user/seller sudah refund
+
+Terima kasih`;
+  }
+};
+
+export {TemplateBugs, TemplateEkspedisi, TemplateCso};
